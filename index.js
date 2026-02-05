80fd748317a5cb63192ae45339ba465fcbd71c89
Hamed-elhageen <hamedragabelhageen@gmail.com>
2026-02-05 13:40:49 +0200

    finishing updated

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import bootstrap from "./src/app.controller.js";
 const app=express();
-// let port=3000;
+let port=3000;
 bootstrap(app,express);
-// app.listen(port,()=>{
-//     console.log("app is running on port "+port)
-// })
+app.listen(port,()=>{
+    console.log("app is running on port "+port)
+})
 
 export default app;
