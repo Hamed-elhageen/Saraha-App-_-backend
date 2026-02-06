9590021bcd66ceb4f9302b960771bb69ab689c43
Hamed-elhageen <hamedragabelhageen@gmail.com>
2026-02-06 19:34:50 +0200

    finish email problems

--- src/Utils/send emails/sendMails.js
+++ src/Utils/send emails/sendMails.js
@@ -1,16 +1,17 @@
+// # when my project was local on my device , for gmail : my device is secure , so it send emails with no problems  , but when the project become on railway , gmail refuse that any outer server send email for security , so on railway i will use brevo which is allowed to send emials, i will send email to him and he will send it to the user
 import nodemailer from 'nodemailer'
 const sendEmail=async({to,subject,html})=>{
     const transporter=nodemailer.createTransport({
-        host:'smtp.gmail.com',
-        port:465,
-        secure:true,
+        host:'smtp-relay.brevo.com',
+        port:587,
+        secure:false,
         auth:{
-            user:process.env.EMAIL,
-            pass:process.env.PASSWORD
+            user:process.env.BREVO_EMAIL,
+            pass:process.env.BREVO_SMTP_KEY
         }
     })
 const info=await transporter.sendMail({
-    from:`Saraha app <${process.env.EMAIL}>`,
+    from:`Saraha app <${process.env.BREVO_EMAIL}>`,
     to,
     subject,
     html
